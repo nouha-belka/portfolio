a81f1bfaf1da4988a9a42f991ead1b0921819cd5
nouha belkacemi <nouhabelka2001@gmail.com>
2025-10-23 19:08:00 -0400

    Hero section doe

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Hero.css"
+import profile_img from "../../assets/profile_img.svg"
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={profile_img} alt=""/>
+      <h1><span>I'm Nouha Belkacemi,</span> I'm a software Engineer and a Cybersecurity Consultant</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus accusamus eaque beatae nulla sed, blanditiis quos! Cumque porro perspiciatis ipsum beatae esse quis ad, aspernatur ullam laudantium. Recusandae, repellat deserunt?</p>
+      <dic class="hero-action">
+        <div class="hero-connect">Connect with me</div>
+        <div class="hero-resume">My resume</div>
+      </dic>
+    </div>
+  )
+}
+
+export default Hero
