486166ddfdbd5ed2003f859d269789c8407fe3b0
nouha belkacemi <nouhabelka2001@gmail.com>
2025-10-30 22:56:11 -0400

    contact form working

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,28 @@ import location_icon from "../../assets/location_icon.svg"
 import call_icon from "../../assets/call_icon.svg"
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "dacc614b-9e7f-49de-910e-f15e5a505aa4");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
   return (
     <div id='contact' className='contact'>
       <div class="contact-title">
@@ -31,7 +53,7 @@ const Contact = () => {
                 </div>
             </div>
         </dic>
-        <form  class="contact-right">
+        <form onSubmit={onSubmit}  class="contact-right">
             <label htmlFor="">Your Name</label>
             <input type="text" placeholder='Enter your name' name='name'/>
             <label htmlFor="">Your email</label>
