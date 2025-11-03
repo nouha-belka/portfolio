0db76415ef06e21e43b242e5906ad3317e9b85c8
nouha belkacemi <nouhabelka2001@gmail.com>
2025-11-03 00:05:47 -0400

    fixing the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,12 +22,12 @@ function Navbar() {
       <ul ref={menuRef} className="nav-menu">
       <img src={menu_close } onClick={closeMenu} alt="" className='nav-mob-close'/>
         <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink> {menu==="home" ?<img src={underline} alt=""></img>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink> {menu==="about" ?<img src={underline} alt=""></img>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink> {menu==="services" ?<img src={underline} alt=""></img>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu("portofolio")}>Portfolio</p></AnchorLink> {menu==="portofolio" ?<img src={underline} alt=""></img>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink> {menu==="contact" ?<img src={underline} alt=""></img>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={100} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink> {menu==="about" ?<img src={underline} alt=""></img>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={100} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink> {menu==="services" ?<img src={underline} alt=""></img>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={100} href='#work'><p onClick={()=>setMenu("portofolio")}>Portfolio</p></AnchorLink> {menu==="portofolio" ?<img src={underline} alt=""></img>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={100} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink> {menu==="contact" ?<img src={underline} alt=""></img>:<></>}</li>
       </ul>
-      <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+      <div className="nav-connect"><AnchorLink className='anchor-link' offset={100} href='#contact'>Connect with me</AnchorLink></div>
     </div>
   )
 }
