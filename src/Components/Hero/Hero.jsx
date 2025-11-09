4960f162c8ee1455fffe6a7db89ee6c85be00f9a
nouha belkacemi <nouhabelka2001@gmail.com>
2025-11-09 01:34:36 -0400

    fixing the links

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,15 +4,18 @@ import profile_img from "../../assets/profile_img.svg"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
+  const handleResumeClick = () => {
+    window.open("https://drive.google.com/file/d/1rUg7H8zOjJ8hwDXPl_x7Pmxc0x8Ivg45/view?usp=sharing", "_blank", "noopener,noreferrer");
+  };
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt=""/>
       <h1><span>I'm Nouha Belkacemi,</span> I'm a software Engineer and a Cybersecurity Consultant</h1>
       <p>Building robust, scalable digital products and securing systems through practical cybersecurity solutions, combining development skills with hands-on penetration testing and auditing.</p>
-      <dic className="hero-action">
+      <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
-      </dic>
+        <div className="hero-resume" onClick={handleResumeClick}>My resume</div>
+      </div>
     </div>
   )
 }
--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -5,6 +5,9 @@ import mywork_data from "../../assets/mywork_data"
 import arrow_icon from "../../assets/arrow_icon.svg"
 
 const MyWork = () => {
+    const handleClick = (link) => {
+    window.open(link, "_blank", "noopener,noreferrer");
+  };
   return (
     <div id='work' className='mywork'>
         <div class="mywork-title">
@@ -13,7 +16,7 @@ const MyWork = () => {
         </div>      
         <div className='mywork-container'>
             {mywork_data.map((work,index)=>{
-                return <img key={index} src={work.w_img} alt=""/>
+                return <img key={index} src={work.w_img} alt="" onClick={() => handleClick(work.w_link)}/>
             })}
         </div>
         <div class="mywork-showmore">
